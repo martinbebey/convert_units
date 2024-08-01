8f96aac478ab0c6bb584ce82b9492b1e9ff0c620
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-08-01 15:24:27 -0400

    added text, boxes, spacers, buttons, alignment, rows and columns

--- app/src/main/java/com/developer/convertunits/MainActivity.kt
+++ app/src/main/java/com/developer/convertunits/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.developer.convertunits
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.developer.convertunits.ui.theme.ConvertUnitsTheme
+import java.time.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +54,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ConvertUnitsTheme {
-        Greeting("Android")
+fun UnitConverter(){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            val context = LocalContext.current
+
+            Box {
+
+                Button(onClick = {
+                    Toast.makeText(context, "dropdown", Toast.LENGTH_LONG).show()
+                }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
+            }
+
+            Box {
+                Button(onClick = {
+                    Toast.makeText(context, "dropdown", Toast.LENGTH_LONG).show()
+                }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Result: ")
     }
+}
+@Preview (showBackground = true)
+@Composable
+fun UnitConverterPreview(){
+    UnitConverter()
 }
